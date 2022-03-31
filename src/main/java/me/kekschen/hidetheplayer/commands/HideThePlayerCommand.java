a3dd0208445d9958b7b1027df51dc4b228caf3a4
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-03-31 22:41:15 +0200

    Added skin changer. Added alias "/htp"

--- src/main/java/me/kekschen/hidetheplayer/commands/HideThePlayerCommand.java
+++ src/main/java/me/kekschen/hidetheplayer/commands/HideThePlayerCommand.java
@@ -8,72 +8,88 @@ import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
 
-public class HideThePlayerCommand implements CommandExecutor, IHidePlayers, IUsernames, IHidePlayerMessages, ICommandHelper {
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(!handlePermission(sender, "rwm.hidetheplayer")) return true;
-        if (args.length == 0) {
-            sendPlayerVisbilitySyntax(sender);
-            sendPlayerNameSyntax(sender);
-            return true;
-        }
-        else if(args.length == 1) {
-            if (args[0].equalsIgnoreCase("player")) sendPlayerVisbilitySyntax(sender);
-            else if(args[0].equalsIgnoreCase("name"))  sendPlayerNameSyntax(sender);
-        }
-        else if(args.length == 2) {
-            if (args[0].equalsIgnoreCase("player")) sendPlayerVisbilitySyntax(sender);
-            else if(args[0].equalsIgnoreCase("name")) sendPlayerNameSyntax(sender);
-        } else if(args.length == 3) {
-            Player p;
-            if((p = handlePlayer(sender, args[2])) == null) return true;
-            if (args[0].equalsIgnoreCase("player")) {
-                if (args[1].equalsIgnoreCase("hide")) {
-                    if (args[2].equalsIgnoreCase("*")) {
-                        hideAllPlayers();
-                        sender.sendMessage(HideThePlayer.PREFIX + "§aAll players are now hidden.");
-                    }
-                    else {
-                        hidePlayer(p);
-                        sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a is now hidden.");
-                    }
-                } else if (args[1].equalsIgnoreCase("show")) {
-                    if (args[2].equalsIgnoreCase("*")) {
-                        showAllPlayers();
-                        sender.sendMessage(HideThePlayer.PREFIX + "§aAll players are now shown.");
-                    }
-                    else {
-                        showPlayer(p);
-                        sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a is now shown.");
-                    }
-                } else sendPlayerVisbilitySyntax(sender);
-            } else if(args[0].equalsIgnoreCase("name")) {
-                if (args[1].equalsIgnoreCase("hide")) {
-                    hideUsername(p);
-                    sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a's name is now hidden.");
-                } else if (args[1].equalsIgnoreCase("show")) {
-                    showUsername(p);
-                    sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a's name is now shown.");
-                } else {
-                    sendPlayerNameSyntax(sender);
-                }
-            }
-        } else if(args.length == 4) {
-            Player p;
-            if((p = handlePlayer(sender, args[2])) == null) return true;
-            if(args[0].equalsIgnoreCase("name")) {
-                if(args[1].equalsIgnoreCase("change")) {
-                    try {
-                        setUsername(p, args[3]);
-                        sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a's name is now §e" + args[3] + "§a.");
-                    } catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                sendPlayerNameSyntax(sender);
-            }
-        }
-        return true;
-    }
+public class HideThePlayerCommand implements CommandExecutor, IHidePlayers, IUsernames, IHidePlayerMessages, ICommandHelper, ISkinInterchangeable {
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (!handlePermission(sender, "rwm.hidetheplayer")) return true;
+		if (args.length == 0) {
+			sendPlayerVisbilitySyntax(sender);
+			sendPlayerNameSyntax(sender);
+			sendPlayerSkinSyntax(sender);
+			return true;
+		} else if (args.length == 1) {
+			if (args[0].equalsIgnoreCase("player")) sendPlayerVisbilitySyntax(sender);
+			else if (args[0].equalsIgnoreCase("name")) sendPlayerNameSyntax(sender);
+			else if (args[0].equalsIgnoreCase("skin")) sendPlayerSkinSyntax(sender);
+		} else if (args.length == 2) {
+			if (args[0].equalsIgnoreCase("player")) sendPlayerVisbilitySyntax(sender);
+			else if (args[0].equalsIgnoreCase("name")) sendPlayerNameSyntax(sender);
+			else if(args[0].equalsIgnoreCase("skin")) {
+				if (!(sender instanceof Player)) {
+					sender.sendMessage(HideThePlayer.PREFIX + "§cYou can only change your skin if you are a player!");
+					return true;
+				}
+				Player p = (Player) sender;
+				changePlayerSkinAsync(sender, p, args[1]);
+			}
+		} else if (args.length == 3) {
+			if (args[0].equalsIgnoreCase("skin")) {
+				if (!(sender instanceof Player)) {
+					sender.sendMessage(HideThePlayer.PREFIX + "§cYou can only change your skin if you are a player!");
+					return true;
+				}
+				Player p2;
+				if ((p2 = handlePlayer(sender, args[1])) == null) return true;
+				changePlayerSkinAsync(sender, p2, args[2]);
+				return true;
+			}
+			Player p;
+			if ((p = handlePlayer(sender, args[2])) == null) return true;
+			if (args[0].equalsIgnoreCase("player")) {
+				if (args[1].equalsIgnoreCase("hide")) {
+					if (args[2].equalsIgnoreCase("*")) {
+						hideAllPlayers();
+						sender.sendMessage(HideThePlayer.PREFIX + "§aAll players are now hidden.");
+					} else {
+						hidePlayer(p);
+						sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a is now hidden.");
+					}
+				} else if (args[1].equalsIgnoreCase("show")) {
+					if (args[2].equalsIgnoreCase("*")) {
+						showAllPlayers();
+						sender.sendMessage(HideThePlayer.PREFIX + "§aAll players are now shown.");
+					} else {
+						showPlayer(p);
+						sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a is now shown.");
+					}
+				} else sendPlayerVisbilitySyntax(sender);
+			} else if (args[0].equalsIgnoreCase("name")) {
+				if (args[1].equalsIgnoreCase("hide")) {
+					hideUsername(p);
+					sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a's name is now hidden.");
+				} else if (args[1].equalsIgnoreCase("show")) {
+					showUsername(p);
+					sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a's name is now shown.");
+				} else {
+					sendPlayerNameSyntax(sender);
+				}
+			}
+		} else if (args.length == 4) {
+			Player p;
+			if ((p = handlePlayer(sender, args[2])) == null) return true;
+			if (args[0].equalsIgnoreCase("name")) {
+				if (args[1].equalsIgnoreCase("change")) {
+					try {
+						setUsername(p, args[3]);
+						sender.sendMessage(HideThePlayer.PREFIX + "§aPlayer §e" + p.getName() + "§a's name is now §e" + args[3] + "§a.");
+					} catch (InvocationTargetException e) {
+						e.printStackTrace();
+					}
+				}
+			} else {
+				sendPlayerNameSyntax(sender);
+			}
+		}
+		return true;
+	}
 }
---
+++ src/main/java/me/kekschen/hidetheplayer/commands/ISkinInterchangeable.java
@@ -0,0 +1,105 @@
+package me.kekschen.hidetheplayer.commands;
+
+import com.comphenix.protocol.wrappers.WrappedGameProfile;
+import com.comphenix.protocol.wrappers.WrappedSignedProperty;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import me.kekschen.hidetheplayer.HideThePlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+class UserSkinData {
+	String name;
+	String value;
+	String signature;
+	public static UserSkinData parseUserSkinData(String skinData) {
+		JsonParser parser = new JsonParser();
+		JsonObject json = parser.parse(skinData).getAsJsonObject();
+		UserSkinData data = new UserSkinData();
+		try {
+		data.name = json.get("username").getAsString();
+		data.value = json.get("textures").getAsJsonObject().get("raw").getAsJsonObject().get("value").getAsString();
+		data.signature = json.get("textures").getAsJsonObject().get("raw").getAsJsonObject().get("signature").getAsString();
+		} catch (NullPointerException e) {
+			return null;
+		}
+		return data;
+	}
+}
+
+public interface ISkinInterchangeable {
+	HashMap<String, UserSkinData> skinDataCache = new HashMap<>();
+	default void changePlayerSkin(CommandSender requester, Player player, String skinName) {
+		// send get request to https://api.ashcon.app/mojang/v2/user/{skinName} and parse the body
+		UserSkinData userSkinData = skinDataCache.getOrDefault(skinName, null);
+		if(userSkinData == null) {
+			URL url;
+			try {
+				// url encode the skin name
+				url = new URL("https://api.ashcon.app/mojang/v2/user/" + URLEncoder.encode(skinName, "UTF-8"));
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+				if (conn.getResponseCode() != 200) {
+					if (conn.getResponseCode() == 404) {
+						player.sendMessage(HideThePlayer.PREFIX + "§cDer Spieler §e" + skinName + " §ckonnte nicht gefunden werden.");
+						return;
+					}
+					throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+				}
+				BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+				String output;
+				StringBuilder sb = new StringBuilder();
+				while ((output = br.readLine()) != null) {
+					sb.append(output);
+				}
+				conn.disconnect();
+				userSkinData = UserSkinData.parseUserSkinData(sb.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (userSkinData == null) {
+				requester.sendMessage(HideThePlayer.PREFIX + "§cThe player §e" + skinName + "§c does not exist.");
+				return;
+			}
+			skinDataCache.put(skinName, userSkinData);
+		}
+
+		WrappedGameProfile profile = WrappedGameProfile.fromPlayer(player);
+		profile.getProperties().remove("textures", profile.getProperties().get("textures").iterator().next());
+		profile.getProperties().put("textures", new WrappedSignedProperty("textures", userSkinData.value, userSkinData.signature));
+		HideThePlayer plugin = HideThePlayer.getPlugin(HideThePlayer.class);
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					p.hidePlayer(plugin, player);
+					p.showPlayer(plugin, player);
+				}
+				requester.sendMessage(HideThePlayer.PREFIX + "§aSuccessfully changed " +
+						(requester.getName().equals(player.getName()) ? "your" : "§e" + player.getName() + "§as") +
+						" skin to §e" + skinName + "§as skin.");
+			}
+		}.runTask(plugin);
+	}
+
+	default void changePlayerSkinAsync(CommandSender requester, Player player, String skinName) {
+		requester.sendMessage(HideThePlayer.PREFIX + "§eLoading skin data...");
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				changePlayerSkin(requester, player, skinName);
+			}
+		}.runTaskAsynchronously(HideThePlayer.getPlugin(HideThePlayer.class));
+	}
+}
